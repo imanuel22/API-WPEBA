8327d3be6864415ece7f70b7d077ee6a5bd6e991
imanuel22 <imanuelrenra220@gmail.com>
2024-10-29 19:50:24 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,8 +29,8 @@ class UserController extends Controller
         $userData['password'] = Hash::make($request->password);
 
         if ($request->hasFile('profile')) {
-            $path = $request->file('profile')->store('images/users', 'public');
-            $userData['profile'] = $path;
+            $profile = $request->file('profile')->store('user', 'public');
+            $userData['profile'] = basename($profile);
         }
 
         $user = User::create($userData);
@@ -46,22 +46,24 @@ class UserController extends Controller
 
     public function update(Request $request, User $user)
     {
+  
         $request->validate([
             'name' => 'string|max:255',
             'email' => 'email|unique:users,email,' . $user->id,
             'role' => 'in:admin,participant,speaker,organizer',
-            'profile' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'profile' => 'nullable|image|mimes:jpg,jpeg,png|max:2048'
         ]);
-
-        $userData = $request->only(['name', 'email', 'role']);
-
+        
+        $userData = $request->only(['name', 'email', 'role','profile']);
         if ($request->hasFile('profile')) {
-            if ($user->image) {
-                Storage::disk('public')->delete($user->image);
+            if ($user->profile) {
+                Storage::disk('public')->delete('user/' . $user->profile);
             }
-            $path = $request->file('profile')->store('images/users', 'public');
-            $userData['profile'] = $path;
+
+            $profilePath = $request->file('profile')->store('user', 'public');
+            $userData['profile'] = basename($profilePath);
         }
+
 
         $user->update($userData);
 
@@ -70,9 +72,9 @@ class UserController extends Controller
 
     public function destroy(User $user)
     {
-        if ($user->image) {
-            Storage::disk('public')->delete($user->image);
-        }
+       if ($user->profile) {
+        Storage::disk('public')->delete('user/' . $user->profile);
+    }
         $user->delete();
 
         return new UserResource(true, 'User Deleted Successfully', null);
