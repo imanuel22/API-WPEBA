88e962cfef361d025830fdc71c712e7b13ce4cf4
imanuel22 <imanuelrenra220@gmail.com>
2024-11-28 15:22:15 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -12,14 +12,20 @@ use App\Http\Controllers\{
     DocumentationController
 };
 
+// Events
 Route::apiResource('events', EventController::class)->only(['index', 'show']);
+// Users
 Route::apiResource('users', UserController::class)->only(['index', 'show']);
+// Category
 Route::apiResource('category', CategoryController::class)->only(['index', 'show']);
+// Registrations
 Route::apiResource('registrations', RegistrationController::class)->only(['index', 'show']);
+// Feedback
 Route::apiResource('feedback', FeedbackController::class)->only(['index', 'show']);
+// Tickets
 Route::apiResource('tickets', TicketController::class)->only(['index', 'show']);
+// Documentation
 Route::apiResource('documentation',DocumentationController::class)->only(['index', 'show']);
-
 // Auth
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'registerParticipant']);
@@ -29,26 +35,19 @@ Route::middleware('jwt')->group(function () {
     // Users
     Route::apiResource('users', UserController::class);
     Route::patch('users/{user}/resetpassword', [UserController::class, 'resetpassword']);
-
     // Events
     Route::apiResource('events', EventController::class);
-
     // Category
     Route::apiResource('category', CategoryController::class);
-
     // Registrations
     Route::apiResource('registrations', RegistrationController::class);
     Route::patch('registrations/verification', [RegistrationController::class, 'verification']);
-
     // Feedback
     Route::apiResource('feedback', FeedbackController::class);
-
     // Tickets
     Route::apiResource('tickets', TicketController::class);
-
     // Documentation
     Route::apiResource('documentation',DocumentationController::class);
-
     // Auth
     Route::post('/register-organizer', [AuthController::class, 'registerOrganizer']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
